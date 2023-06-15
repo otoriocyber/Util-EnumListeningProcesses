b1293030729e36800c5fc5c6f18dfe2898b820fc
Eran Jacob <106976225+e-ot@users.noreply.github.com>
2023-06-15 16:41:11 +0300

    Latest version

--- EnumListeningProcesses.ps1
+++ EnumListeningProcesses.ps1
@@ -6,7 +6,7 @@ This script is an open-source project provided by OTORIO Company for the communi
 This script is designed to help users identify and analyze listening processes on a Windows system. It retrieves information about the processes, such as the process name, ID, and associated ports. Additionally, if available, it fetches details about the services related to those processes.
 
 .PARAMETER OutputFile
-Specifies the path and filename for the output file.
+Specifies the path and filename for the output file (csv format).
 
 .NOTES
 Company: OTORIO LTD
@@ -20,9 +20,9 @@ Company Website: www.otorio.com
 License Information: https://www.gnu.org/licenses/gpl-3.0.html
 
 .EXAMPLE
-	PS C:> .\EnumListeningProcesses.ps1 -OutputFile network_exposed_processes.txt
+	PS C:> .\EnumListeningProcesses.ps1 -OutputFile network_exposed_processes.csv
 OR
-	C:> Powershell -f EnumListeningProcesses.ps1 network_exposed_processes.txt
+	C:> Powershell -f EnumListeningProcesses.ps1 network_exposed_processes.csv
 
 #>
 
@@ -31,19 +31,48 @@ param (
 [string]$OutputFile
 )
 
-$stats = netstat -nao | findstr LISTENING | findstr -v '127.0.0.1'
-$procs = @{}
+$procs = @{}; 
+
+# Collection of listening connections 
+$netstat_raw = netstat -nao # Get-NetTCPConnection may not be supported on some environments
+$stats = $netstat_raw | findstr "LISTENING" | findstr -v '127.0.0.1' 
+$stats += $netstat_raw | findstr "*:*" | findstr -v '127.0.0.1' 
 foreach ($s in $stats){
+	# Aggregate connections per process ID
 	$s_pid = [int]($s -split ' ')[-1]
 	if (-not $procs[$s_pid]) {$procs[$s_pid] = new-object -type System.Collections.ArrayList}
 	$procs[$s_pid].Add($s) | Out-Null
 }
-$services = @{}; Get-CimInstance -class win32_service | foreach {$services[[int]$_.processid]=$_.displayName}
-$procs_data = $procs.keys | foreach{get-process -id $_} | Select-Object ID, Name, Service, Path, ListeningOn | Sort-Object Name
 
-foreach ($p in $procs_data){
-	$p.Service = $services[$p.id]
-	$p.ListeningOn = ($procs[$p.id] | foreach {($_ -split '\s+')[1, 2] -join ' '}) -join ', '
+# Collection of listening UDP connections 
+$stats = netstat -naop UDP  | findstr "*:*" | findstr -v '127.0.0.1' 
+foreach ($s in $stats){
+	# Aggregate connections per process ID
+	$s_pid = [int]($s -split ' ')[-1]
+	if (-not $procs[$s_pid]) {$procs[$s_pid] = new-object -type System.Collections.ArrayList}
+	$procs[$s_pid].Add($s) | Out-Null
 }
 
-$procs_data | Format-Table -AutoSize | Out-File -Width 100000 -FilePath $OutputFile
+# Collection of processes and services details
+$services = @{}; Get-CimInstance -class win32_service | foreach {$services[[int]$_.processid]=$_}
+$procs_data = $procs.keys | foreach{get-process -id $_} | Select-Object ID, Name, Service, Description, Path, ListeningOn | Sort-Object Name
+
+# Aggregating connections per process and adding related service details if exists.
+foreach ($p in $procs_data){
+	if ($services[$p.id]){
+		$p.Service = $services[$p.id].displayName
+		$p.Description = $services[$p.id].Description
+	}
+	if ((-not $p.Description) -and $p.path -and (Test-Path $p.path)){
+		# Collect description based on executatble
+		$p.Description = (get-ChildItem $p.path).VersionInfo.FileDescription
+	}
+	$p.ListeningOn = ($procs[$p.id] | select -Unique | foreach {($_ -split '\s+')[1, 2] -join ' '}) -join ', '
+}
+
+# Writing output to file in CSV format
+try{
+	$procs_data | Export-csv -Path $OutputFile -NoTypeInformation
+}catch{
+	$procs_data | Export-csv -Path $OutputFile
+}
